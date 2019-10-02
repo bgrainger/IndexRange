fcdc54cbfa3e9c3b1afa922e106bdbf821bd0900
Bradley Grainger <bgrainger@gmail.com>
2019-10-02 15:26:19 -0700

    Add unit tests.
    Some test classes were taken from https://github.com/dotnet/corefx/tree/d152d19f0be3dcea1a32f452e9d9940e990574d7/src/System.Runtime/tests/System.

---
+++ tests/IndexRange.UnitTests/AssertExtensions.cs
@@ -0,0 +1,21 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+namespace System
+{
+    public static class AssertExtensions
+    {
+        public static T Throws<T>(string expectedParamName, Func<object> testCode)
+            where T : ArgumentException
+        {
+            T exception = Assert.Throws<T>(testCode);
+
+            Assert.Equal(expectedParamName, exception.ParamName);
+
+            return exception;
+        }
+    }
+}
