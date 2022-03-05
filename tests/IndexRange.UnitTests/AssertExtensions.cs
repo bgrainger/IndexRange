71e40f4076da93a8fa6eaa7119b0f36fb72b8aac
Bradley Grainger <bgrainger@gmail.com>
2022-03-05 11:39:08 -0800

    Use C# 10.

--- tests/IndexRange.UnitTests/AssertExtensions.cs
+++ tests/IndexRange.UnitTests/AssertExtensions.cs
@@ -2,20 +2,17 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Xunit;
+namespace System;
 
-namespace System
+public static class AssertExtensions
 {
-    public static class AssertExtensions
+    public static T Throws<T>(string expectedParamName, Func<object> testCode)
+        where T : ArgumentException
     {
-        public static T Throws<T>(string expectedParamName, Func<object> testCode)
-            where T : ArgumentException
-        {
-            T exception = Assert.Throws<T>(testCode);
+        T exception = Assert.Throws<T>(testCode);
 
-            Assert.Equal(expectedParamName, exception.ParamName);
+        Assert.Equal(expectedParamName, exception.ParamName);
 
-            return exception;
-        }
+        return exception;
     }
 }
--- tests/IndexRange.UnitTests/IndexTests2.cs
+++ tests/IndexRange.UnitTests/IndexTests2.cs
@@ -1,44 +1,40 @@
-using System;
-using Xunit;
+namespace IndexRange.UnitTests;
 
-namespace IndexRange.UnitTests
+public class IndexTests2
 {
-    public class IndexTests2
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(int.MaxValue)]
+    public void ConstructFromBeginning(int value)
     {
-        [Theory]
-        [InlineData(0)]
-        [InlineData(1)]
-        [InlineData(int.MaxValue)]
-        public void ConstructFromBeginning(int value)
-        {
-            Index index = value;
-            Assert.Equal(value, index.Value);
-            Assert.False(index.IsFromEnd);
-        }
+        Index index = value;
+        Assert.Equal(value, index.Value);
+        Assert.False(index.IsFromEnd);
+    }
 
-        [Theory]
-        [InlineData(0)]
-        [InlineData(1)]
-        [InlineData(int.MaxValue)]
-        public void ConstructFromEnd(int value)
-        {
-            Index index = ^value;
-            Assert.Equal(value, index.Value);
-            Assert.True(index.IsFromEnd);
-        }
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(int.MaxValue)]
+    public void ConstructFromEnd(int value)
+    {
+        Index index = ^value;
+        Assert.Equal(value, index.Value);
+        Assert.True(index.IsFromEnd);
+    }
 
-        [Fact]
-        public void ToStringFromBeginning()
-        {
-            Assert.Equal("0", ((Index) 0).ToString());
-            Assert.Equal("100", ((Index) 100).ToString());
-        }
+    [Fact]
+    public void ToStringFromBeginning()
+    {
+        Assert.Equal("0", ((Index) 0).ToString());
+        Assert.Equal("100", ((Index) 100).ToString());
+    }
 
-        [Fact]
-        public void ToStringFromEnd()
-        {
-            Assert.Equal("^0", (^0).ToString());
-            Assert.Equal("^100", (^100).ToString());
-        }
+    [Fact]
+    public void ToStringFromEnd()
+    {
+        Assert.Equal("^0", (^0).ToString());
+        Assert.Equal("^100", (^100).ToString());
     }
 }
